9ae30c55f962a67bb11e223b2bba10108931a26a
lzz73 <131141405+lzz73@users.noreply.github.com>
2024-05-07 11:19:59 +0800

    Update spad.m

--- test_in_matlab/spad.m
+++ test_in_matlab/spad.m
@@ -9,6 +9,6 @@ for i=1:1000                          % the number of testing samples
    x = rgb2ycbcr(x);
    x = x(:,:,1);
    tp= tp+ psnr(x,x_true);
-   ts= ts+ssim(x,x_true);
+   ts= ts+ssim(x*255,x_true*255);
 end
 fprintf('psnr=%6.4f, ssim=%6.8f\n',tp/1000,ts/1000)
